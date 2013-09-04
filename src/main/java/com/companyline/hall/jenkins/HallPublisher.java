a26522d38a0c90d6bdf1c3323f2832e3ebd03f89
Andrew McSherry <andy@andy.local>
2013-09-04 10:07:04 -0700

    Change endpoint to zapier until Jenkins endpoint is published on Hall side

--- src/main/java/com/companyline/hall/jenkins/HallPublisher.java
+++ src/main/java/com/companyline/hall/jenkins/HallPublisher.java
@@ -72,7 +72,7 @@ public class HallPublisher extends Notifier {
 			return true;
 		}
 		
-		URL url = new URL(String.format("https://hall.com/api/1/services/jenkins/%s", api_key));
+		URL url = new URL(String.format("https://hall.com/api/1/services/zapier/%s", api_key));
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		connection.setDoOutput(true);
 		
